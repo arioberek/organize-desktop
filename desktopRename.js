7a5149a3bf2a4593fffc3e21a2bb21eeb867c789
arrbrk1 <acorelado@gmail.com>
2023-02-09 10:14:29 -0300

    first commit

---
+++ desktopRename.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+
+// Change the path to your desktop or any other folder 
+const PATH = "";
+
+const desktopPath = path.join(PATH);
+const files = fs.readdirSync(desktopPath);
+
+for (const file of files) {
+  const filePath = path.join(desktopPath, file);
+  const fileExtension = path.extname(file).toLowerCase();
+  const folderPath = path.join(desktopPath, fileExtension.slice(1));
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+  }
+
+  fs.renameSync(filePath, path.join(folderPath, file));
+}
